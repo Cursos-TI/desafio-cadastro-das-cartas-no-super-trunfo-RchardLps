9988b8918ca069f97358c5d6f4002c23ee9203c9
RchardLps <richardlopes1211@gmail.com>
2025-04-15 09:44:47 +0000

    superTrunfo_desafio_aprendiz

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,37 +1,85 @@
 #include <stdio.h>
 
-int main (){
- 
-  // carta B01(BAHIA - SALVADOR)
- 
- char estado01[20] = "BAHIA", codigo_carta01[5] = "B01", nome_cidade01[20] = "Salvador";
- int populacao01 = 2000000 , pontos_turisticos01 = 25; 
- float area01 = 693.8, pib01 = 62954487.49;
-
- printf("    *SUPER TRUNFO PAISES*\n");
- printf("\n- Estado: %s\n", estado01);
- printf("- Código da carta: %s\n", codigo_carta01);
- printf("- Nome da cidade: %s\n", nome_cidade01);
- printf("- População: %i\n", populacao01);
- printf("- Área: %.2fkm²\n", area01);
- printf("- PIB: R$%.2f\n", pib01);
- printf("- Número de pontos turísticos: %i\n", pontos_turisticos01);
-
-  // carta B02(BAHIA - ITACARÉ)
- 
- char estado02[20] = "BAHIA", codigo_carta02[5] = "B02", nome_cidade02[20] = "Itacaré";
- int populacao02 = 30350, pontos_turisticos02 = 10;
- float area02 = 7262.65, pib02 = 14680.53;
-
- printf("\n- Estado: %s\n", estado02);
- printf("- Código da carta: %s\n", codigo_carta02);
- printf("- Nome da cidade: %s\n", nome_cidade02); 
- printf("- População: %i\n", populacao02);
- printf("- Área: %.2fkm²\n", area02);
- printf("- PIB: R$%.2f\n", pib02);
- printf("- Número de pontos turísticos: %i\n", pontos_turisticos02);
-   
+int main(){
+  
+    /*VARIAVEIS CARTA B01*/
     
-   return 0;
+    char estado01[20], codigo_carta01[5], nome_cidade01[20];
+    int populacao01, pontos_tu01;
+    float area01, pib01;
 
+    printf(" ** Bem vindo ao jogo super trunfo! **\n");
+    
+    /*CARTA B01*/
+
+    printf("Selecione o estado: ");
+      scanf(" %s", estado01);
+    printf("Selecione o codigo da carta: ");
+      scanf("%s", codigo_carta01);
+    printf("Selecione o nome da cidade: ");
+      scanf("%s", nome_cidade01);
+    
+    printf("por favor coloque a população: ");
+      scanf("%d", &populacao01);
+    printf("os pontos turisticos: ");
+      scanf("%d", &pontos_tu01);
+    printf("a área: ");
+      scanf("%f", &area01);
+    printf("o PIB: ");
+      scanf("%f", &pib01);
+
+ 
+  /*informação para o usuario de troca de carta*/
+
+ printf("==== Parabens você terminou uma carta !==== \n Agora vamos para a segunda carta\n");
+    
+   /*VARIAVEIS CARTA B02*/
+   
+   char estado02[20], codigo_carta02[5], nome_cidade02[20];
+   int populacao02, pontos_tu02;
+   float area02, pib02;
+
+    /*CARTA B02*/
+
+    printf("Selecione o estado: ");
+      scanf("%s", estado02);
+    printf("Selecione o codigo da carta: ");
+      scanf("%s", codigo_carta02);
+    printf("Selecione o nome da cidade: ");
+      scanf("%s", nome_cidade02);
+   
+    printf("por favor coloque a população: ");
+      scanf("%d", &populacao02);
+    printf("os pontos turisticos: ");
+      scanf("%d", &pontos_tu02);
+    printf("a área: ");
+      scanf("%f", &area02);
+    printf("o PIB: ");
+      scanf("%f", &pib02);
+
+      /*PARA O TERMINAL */
+    
+ printf("    *SUPER TRUNFO PAISES*\n");
+    
+    /*CARTA B01*/
+    
+    printf("\n- Estado: %s\n", estado01);
+    printf("- Código da carta: %s\n", codigo_carta01);
+    printf("- Nome da cidade: %s\n", nome_cidade01);
+    printf("- População: %dmil\n", populacao01);
+    printf("- Área: %.2fkm²\n", area01);
+    printf("- PIB: R$%.2f\n", pib01);
+    printf("- Número de pontos turísticos: %d\n", pontos_tu01);
+    
+    /*CARTA B02*/
+    
+    printf("\n- Estado: %s\n", estado02);
+    printf("- Código da carta: %s\n", codigo_carta02);
+    printf("- Nome da cidade: %s\n", nome_cidade02); 
+    printf("- População: %i\n", populacao02);
+    printf("- Área: %.2fkm²\n", area02);
+    printf("- PIB: R$%.2f\n", pib02);
+    printf("- Número de pontos turísticos: %i\n", pontos_tu02);
+   
+    return 0;
 }
